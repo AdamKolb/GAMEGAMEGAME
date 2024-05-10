4c796d27bff536280f61a158eb2616ccfac423a5
AdamKolb <142624511+AdamKolb@users.noreply.github.com>
2024-05-10 12:43:22 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,20 +3,4 @@ def game_menu():
   print("1. Rock, Paper, Scissors")
   print("2. Number Guessing Game")
 
-def main():
-  game_menu()
-  choice = input("Enter the number of the game you want to play: ")
 
-  if choice == '1':
-      print("You selected Rock, Paper, Scissors!")
-      # Call function to start Rock, Paper, Scissors game
-  elif choice == '2':
-      print("You selected the Number Guessing Game!")
-      # Call function to start Number Guessing Game?
-  #Add hangman game
-  # Call function to start Hangman game
-  else:
-      print("That is an invalid choice. Please enter 1 or 2.")
-
-if __name__ == "__main__":
-  main()
